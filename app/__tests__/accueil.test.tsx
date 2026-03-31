5d022d370b4a549da2550575d7f3680dfc5aa8f2
Christoooophe <christophealfier02@gmail.com>
2026-03-31 11:46:27 +0200

    add docker

--- app/__tests__/accueil.test.tsx
+++ app/__tests__/accueil.test.tsx
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest";
 
 describe("homepage", () => {
     it("should respond with 200 on /", async () => {
-        const res = await fetch("http://app:3000/");
+        const res = await fetch("http://localhost:3000/");
 
         expect(res.status).toBe(200);
     });
